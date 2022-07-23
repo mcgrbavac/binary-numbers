b27c186e772c4a3eb91f12a62e509459edbf46c8
cgrbavac <cole.grbavac@protonmail.com>
2022-07-22 19:52:40 -0600

    some new code

--- conversionFunctions.py
+++ conversionFunctions.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
+import hashlib
+
 def decimal_to_binary(x):
 	x = int(x)
 	if x == 0:
@@ -41,4 +43,41 @@ def binary_to_decimal(x):
 			storagevalue = storagevalue + value
 		return(storagevalue)
 
+def hexadecimal_to_decimal(x):
+	hexaTable = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f']
+	deciTable = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+	x_length = len(x)
+	x_exponent = x_length - 1
+	x_decimal = 0
+	for i in range(0, x_length):
+		for j in range(0, len(hexaTable)):
+			if x[i] == hexaTable[j]:
+				x_decimal = x_decimal + (deciTable[j] * 16**x_exponent)
+		x_exponent = x_exponent - 1
+	return(x_decimal)
 
+def hexadecimal_to_binary(x):
+	y = hexadecimal_to_decimal(x)
+	z = decimal_to_binary(y)
+	return(z)
+
+def checksum_255(x):
+	x = int(x)
+	x_string = str(x)
+	totalChecksumBits = 8
+	totalHexidecimalLength = 2
+	if x <= totalChecksumBits:
+		return(False)
+	else:
+		x_length = len(x_string)
+		x_start = x_length - totalChecksumBits
+		x_checksumBits = x_string[x_start:x_length]
+		x_inputBits = x_string[0:x_start]
+		x_inputBits_Bits = x_inputBits.encode('utf-8')
+		hashed_input = hashlib.sha256(x_inputBits_Bits).hexdigest()
+		hashed_input_front = hashed_input[0:totalHexidecimalLength]
+		hashed_input_front_binary = hexadecimal_to_binary(hashed_input_front)
+		if x_checksumBits == hashed_input_front_binary:
+			return(True)
+		else:
+			return(False)
--- startConversion.py
+++ startConversion.py
@@ -1,19 +1,57 @@
 #!/usr/bin/python3
 
+from conversionFunctions import *
+
+#Decimal to binary
 answer = input("do you want to convert a decimal number to a binary number? (y/n) ").strip().lower()
 if answer == "y":
 	answer_2 = input("Input a decimal number: ").strip()
-	from conversionFunctions import decimal_to_binary
 	y = decimal_to_binary(answer_2)
 	print(y)
+	exit()
 elif answer == "n":
-	answer = input("Do you want to convert a binary number to a decimal number? (y/n) ").strip().lower()
-	if answer == "y":
-		answer_2 = input("Input a binary number: ").strip()
-		from conversionFunctions import binary_to_decimal
-		y = binary_to_decimal(answer_2)
-		print(y)
-	else:
-		print("Goodbye")
+	answer = ""
 else:
 	print("Goodbye")
+	exit()
+
+#Binary to decimal
+answer = input("Do you want to convert a binary number to a decimal number? (y/n) ").strip().lower()
+if answer == "y":
+	answer_2 = input("Input a binary number: ").strip()
+	y = binary_to_decimal(answer_2)
+	print(y)
+	exit()
+elif answer == "n":
+	answer = ""
+else:
+	print("Goodbye")
+	exit()
+
+#Hexadecimal to decimal
+answer = input("do you want to convert a hexadecimal number to decimal number? (y/n) ").strip().lower()
+if answer == "y":
+	answer_2 = input("Input a hexadecimal number: ").strip()
+	y = hexadecimal_to_decimal(answer_2)
+	print(y)
+	exit()
+elif answer == "n":
+	answer = ""
+else:
+	print("Goodbye")
+	exit()
+
+#Hexadecimal to binary
+answer = input("Do you want to convert a hexadecimal number to a binary number? (y/n) ").strip().lower()
+if answer == "y":
+	answer_2 = input("Input a hexadecimal number: ").strip()
+	y = hexadecimal_to_binary(answer_2)
+	print(y)
+	exit()
+elif answer == "n":
+	answer = ""
+	print("Goodbye")
+	exit()
+else:
+	print("Goodbye")
+	exit()
